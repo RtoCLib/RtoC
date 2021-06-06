6edc3a5d0b5c3816e426082af928ded2f3d75ad4
Donofonos <62661665+Donofonos@users.noreply.github.com>
2021-06-06 13:20:33 -0400

    Update C.c

--- comparacion_R-C/C.c
+++ comparacion_R-C/C.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 //N y M dimenciones de la Matriz
 int n = 2;
 int m = 2;
-void generarMatrizFOR(int Nmax, int **Matriz) {
+int** generarMatriz(int Nmax, int **Matriz) {
   int i, j;
   Matriz = (int **)malloc(n * sizeof(int*)); 
   for (i = 0; i < n; i++) {
@@ -14,50 +15,110 @@ void generarMatrizFOR(int Nmax, int **Matriz) {
       Matriz[i][j] = rand() % (Nmax + 1);
     }
   }
+  return Matriz;
 }
 
-void sumaColumnasFOR(int **Matriz,  int *Vector) {
+int* sumaColumnasFOR(int **Matriz,  int *Vector) {
   int i, j;
   Vector = (int *)malloc(m * sizeof(int)); 
   for (i = 0; i < m; i++) {
     Vector[i] = 0;
     for (j = 0; j < n; j++) {
-      Vector[i] += Matriz[j][i]; // se cae aca por alguna razon
+      Vector[i] += Matriz[j][i];
     }
   }
+  return Vector;
 }
-void liberar(int **M){
-  int i;
-  for (i = 0; i < n; i++) {
-    free(M[i]);
+
+int* sumaColumnasWHILE(int **Matriz,  int *Vector) {
+  int i = 0, j = 0;
+  Vector = (int *)malloc(m * sizeof(int)); 
+  while (i < m) {
+    Vector[i] = 0;
+    while (j < n) {
+      Vector[i] += Matriz[j][i];
+      j++;
+    }
+    i++;
   }
-  free(M);
+  return Vector;
+}
+void liberar(int **Matriz){
+  int i;
+  for (i = 0; i < n; i++)
+    free(Matriz[i]);
+  free(Matriz);
 }
 
 
-time_t funcionFOR(f, c){
-  time_t inicio, fin, S;
+double funcionFOR(f, c){
+  clock_t inicio, fin, S;
   int **M = NULL;
   int *V = NULL;  
 
   n = f;
   m = c;
 
-  generarMatrizFOR(10, M);
+  M = generarMatriz(10, M);
 
-  time(&inicio);
-  sumaColumnasFOR(M,V);
-  time(&fin);
+  inicio = clock();
+  V = sumaColumnasFOR(M,V);
+  fin = clock();
 
   S = fin - inicio;
-  printf("%ld\n", S);
+  //printf("%f\n", (double) (S )/ CLOCKS_PER_SEC);
   liberar(M);
-  return S;
+  return (double) (S )/ CLOCKS_PER_SEC;
 }
 
-int main(){
-  funcionFOR(2,1);
-  printf("%d, %d", n, m);
 
+
+double funcionWHILE(f, c){
+  clock_t inicio, fin, S;
+  int **M = NULL;
+  int *V = NULL;  
+
+  n = f;
+  m = c;
+
+  M = generarMatriz(10, M);
+
+  inicio = clock();
+  V = sumaColumnasWHILE(M,V);
+  fin = clock();
+
+  S = fin - inicio;
+  //printf("%f\n", (double) (S )/ CLOCKS_PER_SEC);
+  liberar(M);
+  return (double) (S )/ CLOCKS_PER_SEC;
+}
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+  int i, j;
+  float PromedioFOR = 0, PromedioWHILE = 0;
+  for (i = 1; i <= 12; i++)
+  {
+    for (j = 0; j < 15; j++)
+    {
+      PromedioFOR += funcionFOR(pow(2,i),pow(2,i+1));
+      PromedioFOR /= 2;
+      PromedioWHILE += funcionWHILE(pow(2,i),pow(2,i+1));
+      PromedioWHILE /= 2;
+    }
+    printf("Promedio (for) iteracion   %d: %f\n", i,PromedioFOR);
+    printf("Promedio (while) iteracion %d: %f\n\n", i,PromedioWHILE);
+    PromedioFOR = 0;
+    PromedioWHILE = 0;
+  }
   return 0;
 }
